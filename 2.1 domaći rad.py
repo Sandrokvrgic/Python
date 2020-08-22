00c06fc5661ed5f43f6304d7ae0f167492b4d2f1
Sandrokvrgic <69734964+Sandrokvrgic@users.noreply.github.com>
2020-08-22 17:07:18 +0200

    Add files via upload

---
+++ 2.1 domaći rad.py
@@ -0,0 +1,24 @@
+print("Hello,I am a converter and I am going to help you convert kilometers into miles. ")
+
+while True :
+
+ kilometri  = float(input("Please enter the amount you would like to convert: " ))
+
+ razlika = 0.621371
+
+ print(kilometri * razlika)
+
+ answer = input("Would you like to do it again?: ")
+
+ if answer =="yes":
+
+    print("Lets do it again!")
+
+ else :
+
+    print("It was nice working for you,hope to see you back in the near future.Goodbye!")
+
+    break
+
+
+
