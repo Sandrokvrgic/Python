48387a339ea2e2459994775be286b621f492fed4
Sandrokvrgic <69734964+Sandrokvrgic@users.noreply.github.com>
2020-08-22 17:08:12 +0200

    Add files via upload

---
+++ 2.3 domaći rad.py
@@ -0,0 +1,4 @@
+
+
+
+print(str.lower("U protekla 24 sata zabilježeno je 306 novih slučajeva zaraze virusom SARS-CoV-2 te je broj trenutno oboljelih (aktivnih slučajeva) u Hrvatskoj danas ukupno 2052."))
